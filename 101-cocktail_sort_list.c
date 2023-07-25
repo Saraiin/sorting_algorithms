d3cdc95fedea66a32dd9e0c348b88e5f4c4edf9a
saraiin <saraennaciri96@gmail.com>
2023-07-25 05:43:18 -0700

    cocktail + quick sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+
+/**
+ * swap_node - swaps two nodes in a douibly linked list
+ * @nd1: the first node to swap
+ * @nd2: the second node to swap
+ * @list: the linked list
+ * @tail: the tail
+ */
+void swap_node(listint_t *nd1, listint_t *nd2, listint_t **list, listint_t **tail)
+{
+	listint_t *prev, *curr, *next;
+
+	prev = nd1->prev;
+	curr = nd1;
+	next = nd2;
+
+	curr->next = next->next;
+	next->prev = prev;
+
+	if (prev)
+		prev->next = next;
+
+	if (next->next)
+		next->next->prev = curr;
+
+	curr->prev = next;
+	next->next = curr;
+
+	if (!(next->prev))
+		*list = next;
+
+	if (!(curr->next))
+		*tail = curr;
+
+	print_list(*list);
+}
+
+/**
+ * cocktail_sort_list - sort a list using the cocktail shaker sort algorithm
+ * @list: The doubly linked list to sort
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *tail, *ptr;
+	int swapped;
+
+	if (!list)
+		return;
+
+	tail = *list;
+	while (tail->next)
+		tail = tail->next;
+
+	do {
+		swapped = 0;
+		ptr = *list;
+		while (ptr)
+		{
+			while (ptr->next && ptr->n > ptr->next->n)
+			{
+				swap_node(ptr, ptr->next, list, &tail);
+				swapped = 1;
+				ptr = ptr->prev;
+			}
+			ptr = ptr->next;
+		}
+		if (!swapped)
+			break;
+
+		swapped = 0;
+		ptr = tail;
+		while (ptr)
+		{
+			while (ptr->prev && ptr->n < ptr->prev->n)
+			{
+				swap_node(ptr->prev, ptr, list, &tail);
+				swapped = 1;
+				ptr = ptr->next;
+			}
+			ptr = ptr->prev;
+		}
+	} while (swapped);
+}
---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+
+
+/**
+ * heap_devide - sorts pivot
+ * @array: array to sort
+ * @start: start index
+ * @end: end index
+ * @size: size of array 
+ * Return: index of pivot
+ */
+size_t hoare_devide(int *array, size_t start, size_t end, size_t size)
+{
+	int pivot = array[end], tmp;
+	size_t i = end + 1, j = start - 1;
+
+	while (1)
+	{
+		do {
+			i--;
+		} while (array[i] > pivot);
+
+		do {
+			j++;
+		} while (array[j] < pivot);
+
+		if (i <= j)
+			return (j);
+
+		tmp = array[j];
+		array[j] = array[i];
+		array[i] = tmp;
+		print_array(array, size);
+	}
+}
+/**
+ * sortHoare - sorts an array using the quick sort algorithm
+ * @array: The array to sort
+ * @start: The start index
+ * @end: The end index
+ * @size: The array size
+ */
+void sortHoare(int *array, int start, int end, size_t size)
+{
+	size_t pivot_index;
+
+	if (start >= end)
+		return;
+
+	pivot_index = hoare_devide(array, start, end, size);
+	sortHoare(array, start, pivot_index - 1, size);
+	sortHoare(array, pivot_index, end, size);
+}
+
+/**
+ * quick_sort_hoare - sorts a list using the quick sort algotithm
+ * @array: The array to  sort
+ * @size: The size of the array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (size < 2 || !array)
+		return;
+
+	sortHoare(array, 0, size - 1, size);
+}
